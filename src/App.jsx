a2e4ef9de55cf032ae68b90c0738e874daaa66a2
Valerius <valeriusdecrypto@gmail.com>
2026-04-13 11:17:17 +0100

    updated footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import About from './components/About'
 import Privacy from './components/privacy'
+import Footer from './components/footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <About />
       <Privacy />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <div className="footer-grid">
+          <div className="footer-brand">
+            <h2>Loan Aptech</h2>
+            <p>Your trusted partner for fast, transparent, and affordable loans solutions.</p>
+          </div>
+
+          <div className="footer-links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/loanproducts">Loan Products</a></li>
+              <li><a href="/apply">Apply Now</a></li>
+              <li><a href="/about">About us</a></li>
+            </ul>
+          </div>
+
+           <div className="footer-links">
+            <h3>Support</h3>
+            <ul>
+              <li><a href="/contact">Contact Us</a></li>
+              <li><a href="/faq">FAQ</a></li>
+              <li><a href="/privacy">Privacy Policy</a></li>
+              <li><a href="/terms">Terms of Service</a></li>
+            </ul>
+          </div>
+
+          <div className="footer-contact">
+         <h3>Get In Touch</h3>
+            <p>support@loanaptech.com</p>
+            <p>Phone: +1 (123) 456-7890</p>
+            <p>Mon-Fri: 9AM-6PM EST </p>
+          </div>
+
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; 2023 MyBrand. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
