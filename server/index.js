603644dbf50544a5433a435507061e63e7b84880
Valerius <valeriusdecrypto@gmail.com>
2026-05-05 08:17:01 +0100

    push changes

--- server/index.js
+++ server/index.js
@@ -28,7 +28,5 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "API is working!" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
