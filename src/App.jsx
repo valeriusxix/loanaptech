d3576552bf0b4603ae0ffa1c2761c93550814f14
Valerius <valeriusdecrypto@gmail.com>
2026-05-08 07:30:54 +0100

    update App.jsx and loandetails.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Signup from "./pages/signup";
 import ApplyLoan from "./pages/applyloan";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
+import LoanDetails from "./pages/loandetails";
 
 export default function App() {
   return (
@@ -33,6 +34,7 @@ export default function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/loan/:id" element= {<LoanDetails/>} />
             </Routes>
           </main>
           <Footer />
--- src/pages/loandetails.jsx
+++ src/pages/loandetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import "./LoanDetails.css";
+import "./loanDetails.css";
 
 function LoanDetails() {
   const { id } = useParams();
